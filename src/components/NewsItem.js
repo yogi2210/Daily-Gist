77102faf398d60f1a1ed7d6bff9f2bbace55679e
Yogesh <yogi.2210@gmail.com>
2022-07-06 11:38:26 +0530

    daily gist first commit

---
+++ src/components/NewsItem.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const NewsItem = (props) => {
+ 
+    let {title, description, imageUrl, newsUrl, author, date } = props;
+    return (
+      <div>
+          <div className="card my-2">
+          <img src= {imageUrl} className="card-img-top" alt="..."/>
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <p className="card-text"><small className="text-muted">By {author} on {new Date(date).toGMTString()}</small></p>
+            <a href={newsUrl} target="_blank" rel="noreferrer" className="btn btn-dark">Read more</a>
+          </div>
+          </div>
+      </div>
+    )
+}
+
+export default NewsItem
